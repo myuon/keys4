359939aee184591229439f4a7be956d6290fd546
myuon <ioi.joi.koi.loi@gmail.com>
2022-10-01 01:50:04 +0900

    chore: graphql with auth

--- app/main.ts
+++ app/main.ts
@@ -1,1 +1,29 @@
-import { graphql } from "@octokit/graphql";
+import { graphql } from "./src/graphql";
+
+const QUERY = `
+  {
+    repository(owner: "octokit", name: "graphql.js") {
+      issues(last: 3, states: [OPEN]) {
+        edges {
+          node {
+            number
+            title
+          }
+        }
+      }
+    }
+  }
+`;
+
+async function main() {
+  try {
+    const { repository } = await graphql(QUERY);
+    for (const { node: issue } of repository.issues.edges) {
+      console.log(`* ${issue.number}: ${issue.title}`);
+    }
+  } catch (err) {
+    console.error(err.message);
+  }
+}
+
+main();
