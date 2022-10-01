226fdbfdb3b455513d1b8b254f55d9dedca132ff
myuon <ioi.joi.koi.loi@gmail.com>
2022-10-01 15:23:11 +0900

    feat: fetch deploy commits and save into sqlite3

--- app/main.ts
+++ app/main.ts
@@ -1,28 +1,86 @@
+import {
+  BranchCommitsDocument,
+  BranchCommitsQuery,
+  BranchCommitsQueryVariables,
+} from "./src/generated/client";
 import { graphql } from "./src/graphql";
+import * as sqlite3 from "sqlite3";
+import dayjs from "dayjs";
+sqlite3.verbose();
 
-const QUERY = `
-  {
-    repository(owner: "octokit", name: "graphql.js") {
-      issues(last: 3, states: [OPEN]) {
-        edges {
-          node {
-            number
-            title
-          }
-        }
-      }
-    }
-  }
-`;
+const db = new sqlite3.Database("./db.sqlite3");
 
 async function main() {
   try {
-    const { repository } = await graphql(QUERY);
-    for (const { node: issue } of repository.issues.edges) {
-      console.log(`* ${issue.number}: ${issue.title}`);
-    }
+    // create a table if not exists
+    db.run(`
+      CREATE TABLE IF NOT EXISTS deployments
+      (
+        hash VARCHAR(50) PRIMARY KEY,
+        url TEXT,
+        created_at BIGINT,
+        repository_name VARCHAR(100)
+      );
+    `);
+
+    const { repository } = await graphql<BranchCommitsQuery>(
+      BranchCommitsDocument.loc?.source.body!,
+      {
+        owner: process.env.OWNER,
+        name: process.env.REPOSITORY,
+        branch: process.env.BRANCH,
+      } as BranchCommitsQueryVariables
+    );
+
+    const commits = repository?.refs?.edges
+      ?.map((edge) => {
+        const target = edge?.node?.target;
+        if (target?.__typename === "Commit") {
+          return target.history.nodes
+            ?.map((node) => {
+              return node
+                ? {
+                    hash: node.oid,
+                    url: node.url,
+                    createdAt: dayjs(node.committedDate).unix(),
+                    repositoryName: `${process.env.OWNER}/${process.env.REPOSITORY}`,
+                  }
+                : undefined;
+            })
+            .filter(
+              (
+                node
+              ): node is {
+                hash: string;
+                url: string;
+                createdAt: number;
+                repositoryName: string;
+              } => Boolean(node)
+            );
+        } else {
+          return undefined;
+        }
+      })
+      .filter(
+        (
+          nodes
+        ): nodes is {
+          hash: string;
+          url: string;
+          createdAt: number;
+          repositoryName: string;
+        }[] => Boolean(nodes)
+      )
+      .flat();
+
+    commits?.forEach((commit) => {
+      db.run(
+        "REPLACE INTO deployments (hash, url, created_at, repository_name) VALUES (?, ?, ?, ?)",
+        [commit.hash, commit.url, commit.createdAt, commit.repositoryName]
+      );
+    });
   } catch (err) {
-    console.error(err.message);
+    console.error(err);
   }
 }
 
