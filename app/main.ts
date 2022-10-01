27e508f994dea80d6996204eff92da40851f35bd
myuon <ioi.joi.koi.loi@gmail.com>
2022-10-01 17:54:54 +0900

    refactor: requestCommitsOnBranch

--- app/main.ts
+++ app/main.ts
@@ -1,13 +1,6 @@
-import {
-  BranchCommitsDocument,
-  BranchCommitsQuery,
-  BranchCommitsQueryVariables,
-} from "./src/generated/client";
-import { graphql } from "./src/graphql";
 import * as sqlite3 from "sqlite3";
-import dayjs from "dayjs";
-import { Deployment } from "./src/models/deployment";
 import { newDeploymentRepository } from "./src/infra/deployment/deployment";
+import { requestCommitsOnBranch } from "./src/api/branchCommit";
 sqlite3.verbose();
 
 const db = new sqlite3.Database("./db.sqlite3");
@@ -17,37 +10,11 @@ async function main() {
   try {
     deploymentRepository.createTableIfNotExists();
 
-    const { repository } = await graphql<BranchCommitsQuery>(
-      BranchCommitsDocument.loc?.source.body!,
-      {
-        owner: process.env.OWNER,
-        name: process.env.REPOSITORY,
-        branch: process.env.BRANCH,
-      } as BranchCommitsQueryVariables
+    const commits = await requestCommitsOnBranch(
+      process.env.OWNER!,
+      process.env.REPOSITORY!,
+      process.env.BRANCH!
     );
-
-    const commits = repository?.refs?.edges
-      ?.map((edge) => {
-        const target = edge?.node?.target;
-        if (target?.__typename === "Commit") {
-          return target.history.nodes
-            ?.map((node) => {
-              return node
-                ? {
-                    hash: node.oid,
-                    url: node.url,
-                    createdAt: dayjs(node.committedDate).unix(),
-                    repositoryName: `${process.env.OWNER}/${process.env.REPOSITORY}`,
-                  }
-                : undefined;
-            })
-            .filter((node): node is Deployment => Boolean(node));
-        } else {
-          return undefined;
-        }
-      })
-      .filter((nodes): nodes is Deployment[] => Boolean(nodes))
-      .flat();
 
     commits?.forEach((commit) => {
       deploymentRepository.save(commit);
---
+++ app/src/api/branchCommit.ts
@@ -0,0 +1,48 @@
+import {
+  BranchCommitsDocument,
+  BranchCommitsQuery,
+  BranchCommitsQueryVariables,
+} from "../generated/client";
+import { graphql } from "./graphql";
+import dayjs from "dayjs";
+import { Deployment } from "../models/deployment";
+
+export const requestCommitsOnBranch = async (
+  owner: string,
+  name: string,
+  branch: string
+) => {
+  const { repository } = await graphql<BranchCommitsQuery>(
+    BranchCommitsDocument.loc?.source.body!,
+    {
+      owner,
+      name,
+      branch,
+    } as BranchCommitsQueryVariables
+  );
+
+  const commits = repository?.refs?.edges
+    ?.map((edge) => {
+      const target = edge?.node?.target;
+      if (target?.__typename === "Commit") {
+        return target.history.nodes
+          ?.map((node) => {
+            return node
+              ? {
+                  hash: node.oid,
+                  url: node.url,
+                  createdAt: dayjs(node.committedDate).unix(),
+                  repositoryName: `${process.env.OWNER}/${process.env.REPOSITORY}`,
+                }
+              : undefined;
+          })
+          .filter((node): node is Deployment => Boolean(node));
+      } else {
+        return undefined;
+      }
+    })
+    .filter((nodes): nodes is Deployment[] => Boolean(nodes))
+    .flat();
+
+  return commits;
+};
