38171d941a7918f02442f347217f871167422ba3
myuon <ioi.joi.koi.loi@gmail.com>
2022-10-02 02:51:37 +0900

    feat: deploy calendar

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,11 +3,43 @@ import dayjs from "dayjs";
 import { css } from "@emotion/react";
 import { useRepository } from "./api/repository";
 import { useDeployment } from "./api/deployment";
-import { Calendar } from "./components/Calendar";
+import { Calendar, useLast7Days } from "./components/Calendar";
+import { useMemo } from "react";
+import { Deployment } from "../../models/deployment";
 
 function App() {
   const { data: repositories } = useRepository();
   const { data: deployments } = useDeployment();
+  const deploysByDate = useMemo(
+    () =>
+      deployments?.reduce((acc, deployment) => {
+        const date = dayjs.unix(deployment.createdAt).format("YYYY-MM-DD");
+        if (!acc[date]) {
+          acc[date] = [];
+        }
+        acc[date].push(deployment);
+        return acc;
+      }, {} as Record<string, Deployment[]>),
+    [deployments]
+  );
+  const deployEvents = useMemo(
+    () =>
+      Object.fromEntries(
+        Object.entries(deploysByDate ?? {}).map(([date, deploys]) => [
+          date,
+          <div
+            css={css`
+              font-weight: bold;
+            `}
+          >
+            🚀 {deploys.length}
+          </div>,
+        ])
+      ),
+    [deploysByDate]
+  );
+
+  const thisWeek = useLast7Days(dayjs());
 
   return (
     <div
@@ -25,7 +57,34 @@ function App() {
           </h2>
         ))}
       </div>
-      <Calendar />
+      <section>
+        <h2>Deployments</h2>
+
+        <div
+          css={css`
+            display: grid;
+            grid-template-columns: repeat(7, 1fr);
+            gap: 8px;
+          `}
+        >
+          {thisWeek.map((day, i) => (
+            <div key={i}>
+              <h3>{day.format("ddd")}</h3>
+              <p>{day.format("M/D")}</p>
+              {deploysByDate?.[day.format("YYYY-MM-DD")] && (
+                <p
+                  css={css`
+                    font-weight: bold;
+                  `}
+                >
+                  🚀 {deploysByDate?.[day.format("YYYY-MM-DD")].length}
+                </p>
+              )}
+            </div>
+          ))}
+        </div>
+      </section>
+      <Calendar events={deployEvents} />
       <div>
         {deployments?.map((d, i) => (
           <div key={i}>
--- web/src/components/Calendar.tsx
+++ web/src/components/Calendar.tsx
@@ -1,6 +1,6 @@
 import { css } from "@emotion/react";
 import dayjs from "dayjs";
-import { useMemo } from "react";
+import React, { useMemo } from "react";
 
 const range = (start: number, end: number) => {
   const length = end - start + 1;
@@ -14,7 +14,7 @@ const useCalendarWeeks = (today: dayjs.Dayjs) => {
     const dayStartOfMonth = startOfMonth.day();
 
     const firstWeek = [
-      ...range(0, dayStartOfMonth - 1).map((_) => "-"),
+      ...range(0, dayStartOfMonth - 1).map((_) => undefined),
       ...range(1, 7 - dayStartOfMonth),
     ];
     const weeks = [firstWeek];
@@ -24,7 +24,7 @@ const useCalendarWeeks = (today: dayjs.Dayjs) => {
       if (date + 6 > endOfMonth.date()) {
         weeks.push([
           ...range(date, endOfMonth.date()),
-          ...range(0, 5 - (endOfMonth.date() - date)).map((_) => "-"),
+          ...range(0, 5 - (endOfMonth.date() - date)).map((_) => undefined),
         ]);
       } else {
         weeks.push(range(date, date + 6));
@@ -38,8 +38,19 @@ const useCalendarWeeks = (today: dayjs.Dayjs) => {
   return weeks;
 };
 
-export const Calendar = () => {
-  const weeks = useCalendarWeeks(dayjs());
+export const useLast7Days = (today: dayjs.Dayjs) => {
+  return useMemo(() => {
+    return range(1, 7).map((i) => today.subtract(i, "day"));
+  }, []);
+};
+
+export const Calendar = ({
+  events,
+}: {
+  events?: Record<string, JSX.Element>;
+}) => {
+  const today = dayjs().add(-1, "month");
+  const weeks = useCalendarWeeks(today);
 
   return (
     <div
@@ -79,7 +90,10 @@ export const Calendar = () => {
         <div key={i}>{d}</div>
       ))}
       {weeks.flat().map((day, i) => (
-        <div key={i}>{day}</div>
+        <div key={i}>
+          {day}
+          {day && events?.[today.set("date", day).format("YYYY-MM-DD")]}
+        </div>
       ))}
     </div>
   );
