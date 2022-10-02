e3892e4df8547ba33a15523a4efaea8b497b2257
myuon <ioi.joi.koi.loi@gmail.com>
2022-10-02 17:54:04 +0900

    refactor: router

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,16 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { SWRConfig } from "swr";
-import App from "./App";
 import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import { routes } from "./pages/routes";
+import { App } from "./App";
+
+const config = {
+  revalidateOnFocus: false,
+};
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <SWRConfig
-      value={{
-        revalidateOnFocus: false,
-      }}
-    ></SWRConfig>
-    <App />
+    <SWRConfig value={config}>
+      <App>
+        <RouterProvider router={routes}></RouterProvider>
+      </App>
+    </SWRConfig>
   </React.StrictMode>
 );
---
+++ web/src/pages/routes.tsx
@@ -0,0 +1,9 @@
+import { IndexPage } from "./IndexPage";
+import { createBrowserRouter } from "react-router-dom";
+
+export const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <IndexPage />,
+  },
+]);
