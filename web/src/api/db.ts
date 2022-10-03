824403e3914813d99a25a21011f66fb6d38638c5
myuon <ioi.joi.koi.loi@gmail.com>
2022-10-04 03:49:03 +0900

    fix: wrong base_url

--- web/src/api/db.ts
+++ web/src/api/db.ts
@@ -2,33 +2,30 @@ import initSqlJs from "sql.js";
 import useSWR from "swr";
 
 export const useDb = () => {
-  const { data, error } = useSWR(
-    `${import.meta.env.BASE_URL}db.sqlite3`,
-    async (url) => {
-      console.log("fetching db");
+  const { data, error } = useSWR("/db.sqlite3", async (url) => {
+    console.log("fetching db");
 
-      const resp = await fetch("/db.sqlite3");
+    const resp = await fetch(`${import.meta.env.BASE_URL}db.sqlite3`);
 
-      console.log("fetched db");
+    console.log("fetched db");
 
-      if (!resp.ok) {
-        console.error("failed to fetch db, ", await resp.text());
-        return;
-      }
-
-      console.log("initializing db");
-
-      const SQL = await initSqlJs({
-        locateFile: (file: string) => `https://sql.js.org/dist/${file}`,
-      });
-
-      const sql = new SQL.Database(new Uint8Array(await resp.arrayBuffer()));
-
-      console.log("db loaded");
-
-      return sql;
+    if (!resp.ok) {
+      console.error("failed to fetch db, ", await resp.text());
+      return;
     }
-  );
+
+    console.log("initializing db");
+
+    const SQL = await initSqlJs({
+      locateFile: (file: string) => `https://sql.js.org/dist/${file}`,
+    });
+
+    const sql = new SQL.Database(new Uint8Array(await resp.arrayBuffer()));
+
+    console.log("db loaded");
+
+    return sql;
+  });
   if (error) {
     console.error("failed to fetch db, ", error);
   }
